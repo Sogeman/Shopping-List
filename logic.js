eae326d231428658e94cd2e7c094e58b6757f1aa
Rene Mandler <32226747+Sogeman@users.noreply.github.com>
2019-03-19 16:25:43 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-    
+    $('#product-table').sortable({
+        stop: () => { // on event stop (drag&drop) clean products/localstorage and read/save them again from list in new order
+            products = [];
+            var items = $(".list-item");
+            items.each((index, listitem) => {
+                var itemName = $(listitem).children().eq(0).html();
+                var count = Number($(listitem).children().eq(1).html());
+                //save status missing
+                console.log($(listitem));
+                Queries.SaveToProducts(itemName, count);
+            });
+            localStorage.clear();
+            Queries.SaveToLocalStorage();
+        }
+    });
     ShoppingList.DrawShoppingList();
     detachedButton = $('#confirm-sorting-button').detach();
     $("#title").text("Einkaufsliste vom " + objDate.getDate() + ". " + month);
